2281d61a32dfa3ab41d886f0feec83f82b19d819
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-10 17:48:32 +0200

    Way Too Long Words

---
+++ src/main/kotlin/a/WayTooLongWords.kt
@@ -0,0 +1,17 @@
+package a
+
+/* Problem Link: https://codeforces.com/problemset/problem/71/A */
+
+fun main() {
+    val n = readln().toInt()
+
+    for (test in 1..n) {
+        val word = readln()
+
+        if (word.length > 10) {
+            println("${word.first()}${word.length - 2}${word.last()}")
+        } else {
+            println(word)
+        }
+    }
+}
