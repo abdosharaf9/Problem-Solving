0bdc17d3a9e5f80c921b5c90570c9e8c15904e44
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-15 15:50:34 +0200

    Beautiful Matrix

--- src/main/kotlin/a/DominoPiling.kt
+++ src/main/kotlin/a/DominoPiling.kt
@@ -1,5 +1,7 @@
 package a
 
+/* Problem Link: https://codeforces.com/problemset/problem/50/A */
+
 fun main() {
     val inputs = readln().split(" ").map { it.toInt() }
     print((inputs[0] * inputs[1]) / 2)
