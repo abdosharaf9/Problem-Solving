379816d9ee1794e31ef804ac0a1ffa4a76cde608
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-18 13:24:47 +0200

    Word Capitalization

---
+++ src/main/kotlin/a/WordCapitalization.kt
@@ -0,0 +1,9 @@
+package a
+
+/* Problem Link: https://codeforces.com/problemset/problem/281/A */
+
+fun main() {
+    var word = readln()
+    word = word.first().uppercase() + word.drop(1)
+    print(word)
+}
