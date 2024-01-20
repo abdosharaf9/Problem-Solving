329cbcfc7c46aca8efdce74c61d43bd8cc8f3bf8
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-20 03:52:37 +0200

    Stones on the Table

---
+++ src/main/kotlin/a/StonesOnTheTable.kt
@@ -0,0 +1,15 @@
+package a
+
+/* Problem Link: https://codeforces.com/problemset/problem/266/A */
+
+fun main() {
+    val n = readln().toInt()
+    val stones = readln()
+    var number = 0
+
+    for (i in 0..stones.length - 2) {
+        if(stones[i] == stones[i+1]) number ++
+    }
+
+    print(number)
+}
