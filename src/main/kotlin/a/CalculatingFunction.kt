5100a984f1009241c5f5d175433407164d8e45b0
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-02-06 02:45:16 +0200

    Calculating Function

---
+++ src/main/kotlin/a/CalculatingFunction.kt
@@ -0,0 +1,8 @@
+package a
+
+/* Problem Link: https://codeforces.com/problemset/problem/486/A */
+
+fun main() {
+    val n = readln().toLong()
+    print(if (n % 2 == 0L) n / 2 else -(n + 1) / 2)
+}
