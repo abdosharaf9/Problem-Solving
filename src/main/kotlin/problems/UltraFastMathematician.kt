7355414713dbdeb31a6f9bb699e8b7d69d3b26c9
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-02-08 18:09:03 +0200

    Ultra-Fast Mathematician

---
+++ src/main/kotlin/problems/UltraFastMathematician.kt
@@ -0,0 +1,11 @@
+package problems
+
+/* Problem Link: https://codeforces.com/problemset/problem/61/A */
+
+fun main() {
+    val first = readln()
+    val second = readln()
+    first.forEachIndexed { index, c ->
+        print(if (c == second[index]) '0' else '1')
+    }
+}
