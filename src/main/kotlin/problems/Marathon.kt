fbd932ff71ab41528e9902396652a73db423823d
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-03-11 23:23:31 +0200

    Marathon

---
+++ src/main/kotlin/problems/Marathon.kt
@@ -0,0 +1,11 @@
+package problems
+
+/* Problem Link: https://codeforces.com/problemset/problem/1692/A */
+
+fun main() {
+    val tests = readln().toInt()
+    for (i in 1..tests) {
+        val distances = readln().split(" ").map { it.toInt() }
+        println(distances.count { it > distances[0] })
+    }
+}
