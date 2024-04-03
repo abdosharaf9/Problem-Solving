54e627207f1f7e5c78f660593f6fb6b3dd28ac2e
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-04-03 08:58:05 +0200

    Yet Another Two Integers Problem

---
+++ src/main/kotlin/problems/YetAnotherTwoIntegersProblem.kt
@@ -0,0 +1,14 @@
+package problems
+
+import kotlin.math.abs
+
+/* Problem Link: https://codeforces.com/problemset/problem/1409/A */
+
+fun main() {
+    var tests = readln().toInt()
+    while (tests-- > 0) {
+        val (a, b) = readln().split(" ").map { it.toInt() }
+        val diff = abs(a - b)
+        println((diff / 10) + if (diff % 10 > 0) 1 else 0)
+    }
+}
