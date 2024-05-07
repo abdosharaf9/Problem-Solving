7738b71b83df4dbb5b68f92203c059cb07c22978
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-05-07 22:30:00 +0300

    Vanya and Cubes

---
+++ src/main/kotlin/problems/VanyaAndCubes.kt
@@ -0,0 +1,19 @@
+package problems
+
+// Problem Link: https://codeforces.com/problemset/problem/492/A
+
+fun main() {
+    val cubes = readln().toInt()
+    var countedCubes = 1
+    var level = 1
+
+    while (countedCubes < cubes) {
+        level++
+        countedCubes += level * (level + 1) / 2
+    }
+
+    print(
+        if (countedCubes > cubes) level - 1
+        else level
+    )
+}
