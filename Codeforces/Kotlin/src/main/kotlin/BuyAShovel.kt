cd30c5456babdabff1b5ff56a16fce188488bd62
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-05-09 06:28:43 +0300

    Buy a Shovel

---
+++ Codeforces/Kotlin/src/main/kotlin/BuyAShovel.kt
@@ -0,0 +1,14 @@
+package problems
+
+// Problem Link: https://codeforces.com/problemset/problem/732/A
+
+fun main() {
+    val (price, coins) = readln().split(" ").map { it.toInt() }
+    var shovels = 1
+
+    while ((shovels * price) % 10 != 0 && (shovels * price) % 10 != coins) {
+        shovels++
+    }
+
+    print(shovels)
+}
