91f21a154f83e4f195a7d112ac3b76172da59b3a
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-05-19 22:37:41 +0300

    Sum of Round Numbers

---
+++ Codeforces/Kotlin/src/main/kotlin/SumOfRoundNumbers.kt
@@ -0,0 +1,28 @@
+package problems
+
+import kotlin.math.pow
+
+// Problem Link: https://codeforces.com/problemset/problem/1352/A
+
+fun main() {
+    var tests = readln().toInt()
+
+    while (tests-- > 0) {
+        var n = readln().toInt()
+
+        var i = 0
+        val numbers = mutableListOf<Int>()
+
+        while (n > 0) {
+            val digit = n % 10
+            n /= 10
+
+            if (digit != 0) numbers.add((digit * 10.0.pow(i)).toInt())
+
+            i++
+        }
+
+        println(numbers.size)
+        println(numbers.joinToString(" "))
+    }
+}
