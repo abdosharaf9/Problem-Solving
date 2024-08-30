4d4cd619ee298d461ed79bad290d97a0b53044cb
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-08-31 00:10:13 +0300

    Plus One

---
+++ Leetcode/Kotlin/src/PlusOne.kt
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/plus-one/
+
+fun plusOne(digits: IntArray): IntArray {
+    var carry = 0
+
+    digits[digits.lastIndex]++
+
+    for (i in digits.lastIndex downTo 0) {
+        val new = digits[i] + carry
+        digits[i] = new % 10
+        carry = new / 10
+    }
+
+    if (carry != 0) {
+        return intArrayOf(carry).plus(digits)
+    }
+
+    return digits
+}
+
+fun printArray(arr: IntArray) {
+    print("[")
+
+    arr.forEachIndexed { idx, x ->
+        print("$x")
+        if (idx != arr.lastIndex) print(", ")
+    }
+
+    println("]")
+}
+
+fun main() {
+    printArray(plusOne(intArrayOf(1, 2, 3)))
+    printArray(plusOne(intArrayOf(4, 3, 2, 1)))
+    printArray(plusOne(intArrayOf(9)))
+    printArray(plusOne(intArrayOf(9, 9)))
+}
