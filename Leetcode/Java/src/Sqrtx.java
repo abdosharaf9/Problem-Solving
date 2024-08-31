680262a23a169ac19851da950f74a85f7c8445ab
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-09-01 02:13:31 +0300

    Sqrt(x)

---
+++ Leetcode/Java/src/Sqrtx.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/sqrtx/
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+
+        int start = 1, end = x, mid;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+
+            if ((long) mid * mid > (long) x) end = mid - 1;
+            else if (mid * mid == x) return mid;
+            else start = mid + 1;
+        }
+
+        return Math.round(end);
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.mySqrt(4));
+        System.out.println(solution.mySqrt(8));
+        System.out.println(solution.mySqrt(2147395599));
+    }
+}
