216f64172d94c4d0245f6c234edaeb30abcd3ab5
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-09-22 06:54:22 +0300

    Weekly Contest 416 - Kotlin

---
+++ Leetcode/Kotlin/src/WeeklyContest416/ReportSpamMessage.kt
@@ -0,0 +1,21 @@
+package WeeklyContest416
+
+class Solution {
+    fun reportSpam(message: Array<String>, bannedWords: Array<String>): Boolean {
+        var count = 0
+        val bannedWordsSet = bannedWords.toSet()
+
+        for (word in message) {
+            if (word in bannedWordsSet) count++
+            if (count == 2) return true
+        }
+
+        return false
+    }
+}
+
+fun main() {
+    val sol = Solution()
+    println(sol.reportSpam(arrayOf("hello", "world", "leetcode"), arrayOf("world", "hello")))
+    println(sol.reportSpam(arrayOf("hello", "programming", "fun"), arrayOf("world", "programming", "leetcode")))
+}
