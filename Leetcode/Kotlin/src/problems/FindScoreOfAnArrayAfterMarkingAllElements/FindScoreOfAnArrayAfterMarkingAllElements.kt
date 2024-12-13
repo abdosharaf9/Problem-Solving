07da85450a4d42f5b5617bf4907d738782817091
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-12-13 21:31:34 +0200

    Find Score of an Array After Marking All Elements - Kotlin #2

--- Leetcode/Kotlin/src/problems/FindScoreOfAnArrayAfterMarkingAllElements/FindScoreOfAnArrayAfterMarkingAllElements.kt
+++ Leetcode/Kotlin/src/problems/FindScoreOfAnArrayAfterMarkingAllElements/FindScoreOfAnArrayAfterMarkingAllElements.kt
@@ -5,9 +5,40 @@ import java.util.PriorityQueue
 // https://leetcode.com/problems/find-score-of-an-array-after-marking-all-elements/
 
 private class Solution {
+    // Time: O(n)
+    // Space: O(1)
+    fun findScore(nums: IntArray): Long {
+        val n = nums.size
+        var score = 0L
+
+        // We loop over the items until we found a small item and then backtrack
+        // with two steps each time to take the items we missed.
+        var l: Int
+        var r = 0
+        while (r < n) {
+            // Start a new window which will start at the end of the previous.
+            l = r
+
+            // Loop to widen the window until we faced a big number.
+            while (r + 1 < n && nums[r + 1] < nums[r]) {
+                r++
+            }
+
+            // Take the numbers inside this window with skipping the neighbors.
+            for (i in r downTo l step 2) {
+                score += nums[i]
+            }
+
+            // To skipp the right neighbor of the current item.
+            r += 2
+        }
+
+        return score
+    }
+
     // Time: O(n * log n)
     // Space: O(n)
-    fun findScore(nums: IntArray): Long {
+    /*fun findScore(nums: IntArray): Long {
         val n = nums.size
 
         // Stores the items with its index to easily get the smallest each time,
@@ -39,7 +70,7 @@ private class Solution {
         }
 
         return score
-    }
+    }*/
 }
 
 fun main() {
